d5b501b38258cebf4c4f0b368137058c338c782f
dorsariahi <dorsariahiasl@gmail.com>
2023-02-15 12:39:47 -0600

    added id because of error

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,21 +1,21 @@
-INSERT INTO department (name)
-VALUES ("legal"),
-       ("engineering"),
-       ("management"),
-       ("new hires");
+INSERT INTO department (id, name)
+VALUES (1, "legal"),
+       (2, "engineering"),
+       (3, "management"),
+       (4, "new hires");
        
-INSERT INTO role (title, salary, department_id)
-VALUES ("sales person", 10000, 1),
-        ("admin", 35000, 2 ),
-        ("engineer", 75000, 3),
-        ("reseption", 20000, 4);
+INSERT INTO role (id, title, salary, department_id)
+VALUES (1, "sales person", 10000, 1),
+        (2, "admin", 35000, 2 ),
+        (3, "engineer", 75000, 3),
+        (4, "reseption", 20000, 4);
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Melissa", "Robert", 1, NULL)
-        ("Ali", "Kashani", 4, 1)
-        ("Rock", "Miller", 2, 1)
-        ("Nola", "Moore", 3, 1)
-        ("Nikki", "White", 3, 1)
-        
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (1, "Melissa", "Robert", 1, NULL),
+        (2, "Ali", "Kashani", 4, 1),
+        (3, "Rock", "Miller", 2, 1),
+        (4, "Nola", "Moore", 3, 1),
+        (5, "Nikki", "White", 3, 1);
+
 
        
