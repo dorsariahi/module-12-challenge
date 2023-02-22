87b4d855c5e86a59297969fb64ea4a78317e2747
dorsariahi <dorsariahiasl@gmail.com>
2023-02-21 19:01:14 -0600

    added the if statement

--- index.js
+++ index.js
@@ -20,13 +20,36 @@ function startApp() {
             type: "list",
             name: "mainMenu",
             message: "what would you like to do?",
-            choices: ["view all departments", "view all roles", "view all employees", "add a department", "add a role", "add an employee", "update an employee role"]
+            choices: ["view all departments", "view all roles", "view all employees", "add a department", "add a role", "add an employee", "Change employee role", "Change employe manager"]
         }
-    ]).then(function(response){
-        console.log(response)
-        process.exit();
+    ]).then((input) => {
+        const { options } = input;
+        if (options === "view all departments") {
+            getDepartment();
+        }
+        if (options === "view all roles") {
+            getRoles();
+        }
+        if (options === "view all employees") {
+            getEmployee();
+        }
+        if (options === "add a department") {
+            newDepartment();
+        }
+        if (options === "add a role") {
 
-    })
+        }
+        if (options === "add an employee") {
+
+        }
+        if (options === "Change employee role") {
+
+        }
+        if (options === "Change employe manager") {
+
+        }
+    }
+    )
 
 }
 startApp()
